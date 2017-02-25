c6c302cc1126ce616deb49a74f495ddd9804d440
BOPOHOB <self@bopohob.ru>
2017-02-25 19:07:17 +0300

    iss #5

--- frontend/src/app/toolbox/label-grade/label-grade.component.ts
+++ frontend/src/app/toolbox/label-grade/label-grade.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
 
+declare var isNaN: any;
+
 @Component({
   selector: 'label-grade',
   template: `
   <div class="dialogue-tbl-progress">
-  	<div [style.background-color]="grade | gradeToColor">{{ grade | number:'1.0-1' }}</div>
+  	<div [style.background-color]="grade | gradeToColor">{{ (isNaN(grade) ? '—' : (grade | number:'1.0-1')) }}</div>
   </div>`,
   styleUrls: ['./label-grade.component.css']
 })
 export class LabelGradeComponent  {
+  isNaN() : boolean { return isNaN(arguments[0]); }
+
   @Input() grade : number;
 }
